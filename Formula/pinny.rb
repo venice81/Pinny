f6c222a2312bee7dbe2d10f260d7d81462008ef0
Byungwoong Park <venice81@gmail.com>
2026-03-17 23:44:52 +0900

    brew: update formula for 0.1.2

--- Formula/pinny.rb
+++ Formula/pinny.rb
@@ -3,8 +3,8 @@ class Pinny < Formula
 
   desc "TUI/CLI wrapper for xcrun simctl location"
   homepage "https://github.com/venice81/Pinny"
-  url "https://github.com/venice81/Pinny/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "806c783700958e53ca929bb2a310c1186270d683955730ed676b8b881a24e63d"
+  url "https://github.com/venice81/Pinny/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "5a9624f6777fcc7d44c5453b99e350c744de3729cc1dd02f4d9d8975627e50aa"
   license "MIT"
 
   depends_on "python@3.12"
